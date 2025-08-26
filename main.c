e214caf134d86fa4f95e9efec90e8abe1399d6f1
singhpratap1055-lgtm <singhpratap1055@gmail.com>
2025-08-26 11:34:03 +0530

    daily program 1

---
+++ main.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("enter a number");
+    scanf("%d", &a);
+    printf("enter a number");
+    scanf("%d", &b);
+    c=a+b;
+    printf("the sum of c is %d",c);
+    return 0;
+}
+
+    
+        
+        
+     
+    
+    
+
+    
+    
