e03df348f6329879aa8313ea8d0b09de7917096a
singhpratap1055-lgtm <singhpratap1055@gmail.com>
2025-08-26 14:52:35 +0530

    Day1 program02

---
+++ main2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("enter a no ");
+    scanf("%d", &a);
+    printf("enter a no");
+    scanf("%d", &b);
+    printf("the sum of two no,difference,product,quotient is %d %d %d %d",c=a+b,c=a*b,c=a-b,c=a/b);
+    return 0;
+}
+
+
+
