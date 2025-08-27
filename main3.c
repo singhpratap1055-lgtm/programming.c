32977d323ecbb17429eaa2cbddac1dabd0b31369
singhpratap1055-lgtm <singhpratap1055@gmail.com>
2025-08-27 15:46:52 +0530

    day-2 programm-1

---
+++ main3.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int main(){
+    int length,breadth,c;
+    printf("enter a no");
+    scanf("%d" ,&length);
+    printf("enter a no");
+    scanf("%d", &breadth);
+   
+    printf("the area of rectangle and perimeter is %d %d",c=length*breadth ,c=2*(length+breadth));
+    return 0;
+    
+    
+  
+    
+}
